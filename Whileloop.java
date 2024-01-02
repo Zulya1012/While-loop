c37aefe5db9b1a40182e85751484139392ea220d
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-02 22:05:49 +0400

    Update Whileloop.java

--- Whileloop.java
+++ Whileloop.java
@@ -1,10 +1,10 @@
-public class Whileloop{
-public static void main(String[] arg){
-int i=1;
-while(i<=10){
-i=i+2;
-System.out.println(i);
+public class Whileloop {
+  public static void main(String[] arg) {
+    int i = 1;
+    while (i <= 10) {
+      i = i + 2;
+      System.out.println(i);
 
-}
-}
+    }
+  }
 }
